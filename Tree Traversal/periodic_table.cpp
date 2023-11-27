b48f438246f72f9dabdcacabb11d9ee512b93e28
Saidul Islam Rajib <saidul.bd@asa-international.com>
2023-11-27 11:26:57 +0600

    add periodic table

---
+++ Tree Traversal/periodic_table.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	int n;
+	cout << "Enter the number your want : ";
+	cin >> n;
+
+	for(int i{1}; i<=10; i++){
+		cout << n << " X " << i << " = " << n*i << endl;
+	}
+}
